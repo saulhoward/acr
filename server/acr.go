49beb48fe883882be72a7a0563b78ead98c43fb3
Saul Howard <saul@saulhoward.com>
2015-06-19 19:06:08 +0100

    add web client code

--- server/acr.go
+++ server/acr.go
@@ -7,7 +7,10 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/saulhoward/acr/github"
+	"github.com/saulhoward/acr/server/github"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
 )
 
 var (
@@ -49,6 +52,42 @@ func main() {
 	// mux := http.NewServeMux()
 	// mux.HandleFunc("/", listReposHandler)
 	// http.ListenAndServe(":8083", mux)
+
+	r := gin.Default()
+	r.LoadHTMLFiles("./client/static/index.html")
+	r.Static("/js", "./client/static/js")
+	r.Static("/css", "./client/static/css")
+
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(200, "index.html", nil)
+	})
+
+	r.GET("/ws", func(c *gin.Context) {
+		wshandler(c.Writer, c.Request)
+	})
+
+	r.Run("localhost:12312")
+}
+
+var wsupgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func wshandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := wsupgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println("Failed to set websocket upgrade: %+v", err)
+		return
+	}
+
+	for {
+		t, msg, err := conn.ReadMessage()
+		if err != nil {
+			break
+		}
+		conn.WriteMessage(t, msg)
+	}
 }
 
 func listReposHandler(w http.ResponseWriter, r *http.Request) {
